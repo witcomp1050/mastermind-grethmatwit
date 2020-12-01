8e54843a306c885b190a809868cdfd0947903dc6
grethmatwit <grethm@wit.edu>
2020-12-01 16:29:01 -0500

    testing the blank and duplicate options now

--- src/edu/wit/comp1050/mastermind.java
+++ src/edu/wit/comp1050/mastermind.java
@@ -4,14 +4,11 @@ import java.util.Random;
 
 class mastermind{
    public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in).useDelimiter( "(\\b|\\B)" ) ;
        for(int z=0;z<1;){
-           System.out.printf("%nCan the CodeMaker use duplicate colors? (y/n): ");
-           String duplicate = input.next();
-           System.out.printf("%nCan the CodeMaker use blank spaces? (y/n): ");
-           String blank = input.next();
 
-           String Answer = getColorAnswer(duplicate,blank);
+
+           String Answer = getColorAnswer();
 
            System.out.printf("%nAnswer: " + Answer);
 
@@ -75,35 +72,196 @@ class mastermind{
 
     }
 
-    public static String getColorAnswer(String duplicate, String blank){
+    public static String getColorAnswer(){
 
-       //generate array
-       char [] colorArray = new char[]{'R','O','Y','G','B','P'};
-       /*
-       colorArray[0] = 'R';
-       colorArray[1] = 'O';
-       colorArray[2] = 'Y';
-       colorArray[3] = 'G';
-       colorArray[4] = 'B';
-       colorArray[5] = 'P';
+        Scanner input = new Scanner(System.in).useDelimiter( "(\\b|\\B)" ) ;
 
-        */
-       //generate answer
+        System.out.printf("%nUse default settings? (y/n)= ");
+        //can use duplicates but no spaces
+        String defaultSetting = input.next();
+        defaultSetting = defaultSetting.toUpperCase();
 
-        Random rand = new Random();
-        int a = rand.nextInt(5);
-        int b = rand.nextInt(5);
-        int c = rand.nextInt(5);
-        int d = rand.nextInt(5);
+        //generate array
+        char [] colorArray = new char[]{'R','O','Y','G','B','P',' '};
 
-        char A = colorArray[a];
-        char B = colorArray[b];
-        char C = colorArray[c];
-        char D = colorArray[d];
+        if(defaultSetting.charAt(0) == 'Y'){
+            /*
+            colorArray[0] = 'R';
+            colorArray[1] = 'O';
+            colorArray[2] = 'Y';
+            colorArray[3] = 'G';
+            colorArray[4] = 'B';
+            colorArray[5] = 'P';
+
+            */
+            //generate answer
+
+            Random rand = new Random();
+            int a = rand.nextInt(5);
+            int b = rand.nextInt(5);
+            int c = rand.nextInt(5);
+            int d = rand.nextInt(5);
+
+            char A = colorArray[a];
+            char B = colorArray[b];
+            char C = colorArray[c];
+            char D = colorArray[d];
 
 
 
-        return (""+A+""+B+""+C+""+D);
+            return (""+A+""+B+""+C+""+D);
+
+        }
+        if (defaultSetting.charAt(0)=='N'){
+            System.out.printf("%nCan the CodeMaker use duplicate colors? (y/n): ");
+            String duplicate = input.next();
+            duplicate = duplicate.toUpperCase();
+
+            System.out.printf("%nCan the CodeMaker use blank spaces? (y/n): ");
+            String blank = input.next();
+            blank = blank.toUpperCase();
+
+
+            /*
+            colorArray[0] = 'R';
+            colorArray[1] = 'O';
+            colorArray[2] = 'Y';
+            colorArray[3] = 'G';
+            colorArray[4] = 'B';
+            colorArray[5] = 'P';
+            colorArray[6] = ' ';
+
+            */
+            //generate answer
+            if ((blank.charAt(0)=='Y')&&(duplicate.charAt(0)=='Y')){
+
+                Random rand = new Random();
+                int a = rand.nextInt(6);
+                int b = rand.nextInt(6);
+                int c = rand.nextInt(6);
+                int d = rand.nextInt(6);
+
+                char A = colorArray[a];
+                char B = colorArray[b];
+                char C = colorArray[c];
+                char D = colorArray[d];
+
+                return (""+A+""+B+""+C+""+D);
+            }
+            if((blank.charAt(0)=='N')&&(duplicate.charAt(0)=='N')){
+            /*
+            colorArray[0] = 'R';
+            colorArray[1] = 'O';
+            colorArray[2] = 'Y';
+            colorArray[3] = 'G';
+            colorArray[4] = 'B';
+            colorArray[5] = 'P';
+
+            */
+                //generate answer
+
+                Random rand = new Random();
+                int a = rand.nextInt(5);
+                int b = rand.nextInt(5);
+                while(b==a){
+                    b = rand.nextInt(5);
+                }
+                int c = rand.nextInt(5);
+                while((c==b)||c==a){
+                    c = rand.nextInt(5);
+                }
+                int d = rand.nextInt(5);
+                while ((d==a)||(d==b)||(d==c)){
+                    d = rand.nextInt(5);
+                }
+
+                char A = colorArray[a];
+                char B = colorArray[b];
+                char C = colorArray[c];
+                char D = colorArray[d];
+
+
+
+                return (""+A+""+B+""+C+""+D);
+
+            }
+            if((blank.charAt(0)=='N')&&(duplicate.charAt(0)=='Y')){
+            /*
+            colorArray[0] = 'R';
+            colorArray[1] = 'O';
+            colorArray[2] = 'Y';
+            colorArray[3] = 'G';
+            colorArray[4] = 'B';
+            colorArray[5] = 'P';
+
+
+            */
+                //generate answer
+
+                Random rand = new Random();
+                int a = rand.nextInt(5);
+                int b = rand.nextInt(5);
+                int c = rand.nextInt(5);
+                int d = rand.nextInt(5);
+
+                char A = colorArray[a];
+                char B = colorArray[b];
+                char C = colorArray[c];
+                char D = colorArray[d];
+
+
+
+                return (""+A+""+B+""+C+""+D);
+
+            }
+            if((blank.charAt(0)=='Y')&&(duplicate.charAt(0)=='N')){
+            /*
+            colorArray[0] = 'R';
+            colorArray[1] = 'O';
+            colorArray[2] = 'Y';
+            colorArray[3] = 'G';
+            colorArray[4] = 'B';
+            colorArray[5] = 'P';
+            colorArray[6] = ' ';
+
+            */
+                //generate answer
+
+                Random rand = new Random();
+                int a = rand.nextInt(6);
+                int b = rand.nextInt(6);
+                while(b==a){
+                    b = rand.nextInt(6);
+                }
+                int c = rand.nextInt(6);
+                while((c==b)||c==a){
+                    c = rand.nextInt(6);
+                }
+                int d = rand.nextInt(6);
+                while ((d==a)||(d==b)||(d==c)){
+                    d = rand.nextInt(6);
+                }
+
+                char A = colorArray[a];
+                char B = colorArray[b];
+                char C = colorArray[c];
+                char D = colorArray[d];
+
+
+
+                return (""+A+""+B+""+C+""+D);
+
+            }
+            else{
+                System.out.printf("%nInvalid settings");
+                getColorAnswer();
+            }
+
+        }
+        System.out.printf("%nInvalid settings");
+        getColorAnswer();
+        return null;
+
     }
 
 }
